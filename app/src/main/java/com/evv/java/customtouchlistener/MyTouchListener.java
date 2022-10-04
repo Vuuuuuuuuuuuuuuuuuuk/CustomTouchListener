f4541c931bfc8ca8a465f1eea4cb40ab3223e9d6
Vuuuuuuuuuuuuuuuuuuk <baberlaybabs@gmail.com>
2022-10-04 10:06:30 +0300

    part3

--- app/src/main/java/com/evv/java/customtouchlistener/MyTouchListener.java
+++ app/src/main/java/com/evv/java/customtouchlistener/MyTouchListener.java
@@ -13,11 +13,13 @@ public class MyTouchListener implements View.OnTouchListener {
   Context context;
   TextView tvOut;
 
-  boolean isPressed;
-  boolean isLongClicked;
+  boolean isMoved = false;
+  boolean isPressed = false;
+  boolean isLongClicked = false;
   int timeDelay = 1000;
   int countOfHandlers = 0;
 
+  float dif  = 2.0f;
   float xS[] = new float[10];
   float yS[] = new float[10];
 
@@ -47,13 +49,14 @@ public class MyTouchListener implements View.OnTouchListener {
         xS[pointerID] = event.getX(pointerIndex);
         yS[pointerID] = event.getY(pointerIndex);
 
+        isMoved   = false;
         isPressed = true;
         isLongClicked = false;
         countOfHandlers++;
         new Handler().postDelayed(new Runnable() {
           @Override
           public void run() {
-            if(--countOfHandlers == 0 && isPressed == true){
+            if(--countOfHandlers == 0 && isPressed == true && isMoved == false){
               tvOut.setText("LongClick");             //do action for long click
               isLongClicked = true;
             }
@@ -67,16 +70,25 @@ public class MyTouchListener implements View.OnTouchListener {
       case MotionEvent.ACTION_CANCEL:
 
         isPressed = false;
-        if(!isLongClicked){
+        if(!isLongClicked && !isMoved){
           isLongClicked = false;
           if(yS[0] <= maxY/3) tvOut.setText("section 1");
           else if(yS[0] <= 2*maxY/3) tvOut.setText("section 2");
           else tvOut.setText("#");
         }
+        isMoved   = false;
 
         break;
 
       case MotionEvent.ACTION_MOVE:
+        if(isMoved == true){
+          tvOut.setText("Move : " + event.getX(pointerIndex) + " " + event.getY(pointerIndex));
+        }else{
+          if( Math.abs(event.getX(pointerIndex) - xS[0] ) > dif || Math.abs(event.getY(pointerIndex) - yS[0] ) > dif ){
+            isMoved = true;
+            tvOut.setText("Move : " + event.getX(pointerIndex) + " " + event.getY(pointerIndex));
+          }
+        }
         break;
     }
 
