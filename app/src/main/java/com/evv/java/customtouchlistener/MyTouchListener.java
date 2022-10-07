4892596bed52610d8978ea86253be5d20531c1e4
Vuuuuuuuuuuuuuuuuuuk <baberlaybabs@gmail.com>
2022-10-07 11:58:44 +0300

    part4

--- app/src/main/java/com/evv/java/customtouchlistener/MyTouchListener.java
+++ app/src/main/java/com/evv/java/customtouchlistener/MyTouchListener.java
@@ -7,24 +7,28 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MyTouchListener implements View.OnTouchListener {
   Context context;
   TextView tvOut;
 
+  boolean isSwiped = false;
   boolean isMoved = false;
   boolean isPressed = false;
   boolean isLongClicked = false;
   int timeDelay = 1000;
   int countOfHandlers = 0;
 
-  float dif  = 2.0f;
+  float dif  = 5.0f;
   float xS[] = new float[10];
   float yS[] = new float[10];
 
   private int maxX;
   private int maxY;
+
+  long difTime = 100;
 
   public MyTouchListener(Context context, TextView tvOut) {
     this.context = context;
@@ -49,6 +53,7 @@ public class MyTouchListener implements View.OnTouchListener {
         xS[pointerID] = event.getX(pointerIndex);
         yS[pointerID] = event.getY(pointerIndex);
 
+        isSwiped  = false;
         isMoved   = false;
         isPressed = true;
         isLongClicked = false;
@@ -56,7 +61,7 @@ public class MyTouchListener implements View.OnTouchListener {
         new Handler().postDelayed(new Runnable() {
           @Override
           public void run() {
-            if(--countOfHandlers == 0 && isPressed == true && isMoved == false){
+            if(--countOfHandlers == 0 && isPressed == true && isMoved == false && isSwiped == false){
               tvOut.setText("LongClick");             //do action for long click
               isLongClicked = true;
             }
@@ -70,24 +75,37 @@ public class MyTouchListener implements View.OnTouchListener {
       case MotionEvent.ACTION_CANCEL:
 
         isPressed = false;
-        if(!isLongClicked && !isMoved){
+        if(!isLongClicked && !isMoved && !isSwiped){
           isLongClicked = false;
           if(yS[0] <= maxY/3) tvOut.setText("section 1");
           else if(yS[0] <= 2*maxY/3) tvOut.setText("section 2");
           else tvOut.setText("#");
         }
+        else if(isSwiped){
+          float dx = event.getX(pointerIndex) - xS[0];                         //SWIPE !!!
+          if(dx > 0) tvOut.setText("Swipe toward right");
+          else tvOut.setText("Swipe without action");
+        }
+
         isMoved   = false;
+        isSwiped  = false;
 
         break;
 
       case MotionEvent.ACTION_MOVE:
+
+        if(isLongClicked || isSwiped) break;
+
         if(isMoved == true){
-          tvOut.setText("Move : " + event.getX(pointerIndex) + " " + event.getY(pointerIndex));
-        }else{
+          tvOut.setText("Move : " + event.getX(pointerIndex) + " " + event.getY(pointerIndex));  //correct !!!
+        }
+        else{
           if( Math.abs(event.getX(pointerIndex) - xS[0] ) > dif || Math.abs(event.getY(pointerIndex) - yS[0] ) > dif ){
-            isMoved = true;
-            tvOut.setText("Move : " + event.getX(pointerIndex) + " " + event.getY(pointerIndex));
-          }
+            long tempDifTime = event.getEventTime() - event.getDownTime();
+
+            if(tempDifTime <= difTime) isSwiped = true;
+            else isMoved = true;
+            }
         }
         break;
     }
